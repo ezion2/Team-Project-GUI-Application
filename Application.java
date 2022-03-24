33bfdf7c7b5a2e004019903ffa0f1910bfa05478
StephenLubitz <66440852+StephenLubitz@users.noreply.github.com>
2022-03-24 15:59:08 -0700

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,41 @@
+// CSE 360
+// Team Project - Admin Class
+// Brandon Nosaka, Stephen Lubitz, Zion Esemonu, Jeffrey Lafleur
+// This program creates the Admin Class for the team project. The Admin is allowed to distribute coupons to customers 
+// and update the menu.
+
+import java.io.*;
+import java.util.*;
+
+public class Application
+{
+		
+	protected User[] user;
+	public User[] getUser() 
+	{
+		return this.user;
+	}	
+	
+	public boolean verifyLogin(User user, String userName, String password) {
+		if (userName.equals(user.userName)) {
+			if(password.equals(user.password)) {
+				return true;
+			}
+			
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+}
+		
+			
+	//public addToQueue() {
+			
+		//}
+	//public menu() {
+	//}	
+//}
