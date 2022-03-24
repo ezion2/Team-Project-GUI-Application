54b0523139e62822c4d6c777caf3771590677359
BNosaka <76081931+BNosaka@users.noreply.github.com>
2022-03-24 15:47:51 -0700

    Created User and Admin Classes
    Admin not finished yet.

---
+++ Admin.java
@@ -0,0 +1,46 @@
+// CSE 360
+// Team Project - Admin Class
+// Brandon Nosaka, Stephen Lubitz, Zion Esemonu, Jeffrey Lafleur
+// This program creates the Admin Class for the team project. The Admin is allowed to distribute coupons to customers 
+// and update the menu.
+
+public class Admin extends User{
+	
+	// default constructor
+	Admin() {
+		
+		// initialize an empty admin
+		this.userName = null;
+		this.email = null;
+		this.password = null;
+	}
+	
+	
+	// overloaded constructor
+	Admin(String userName, String email, String password){
+		
+		// initialize an admin
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+	// distributeCoupon method
+	public void distributeCoupon(EndUser customer) {
+		
+		// create variables needed
+		boolean coupon = true;
+		
+		// give the coupon to the customer
+		customer.setCoupon(coupon);
+	}
+	
+	
+	// updateMenu method
+	public void updateMenu() {
+		
+		// ************************************************************************
+		// finishing later
+	}
+}
---
+++ User.java
@@ -0,0 +1,57 @@
+// CSE 360
+// Team Project - User Class
+// Brandon Nosaka, Stephen Lubitz, Zion Esemonu, Jeffrey Lafleur
+// This program creates the User Class for the team project. The User has their own profile and can login, logout, and register.
+
+public class User {
+	
+	
+	// declare properties needed
+	protected String userName;
+	protected String email;
+	protected String password;
+	
+	
+	// default constructor
+	User(){
+		
+		// initialize an empty user
+		this.userName = null;
+		this.email = null;
+		this.password = null;
+	}
+	
+	
+	// overloaded constructor
+	User(String userName, String email, String password){
+		
+		// initialize a user
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+	// getUserName method
+	public String getUserName(){
+		
+		// return the user's userName
+		return this.userName;
+	}
+	
+	
+	// getEmail method
+	public String getEmail() {
+		
+		// return the user's email
+		return this.email;
+	}
+	
+	
+	// getPassword method
+	public String getPassword() {
+		
+		// return the user's password
+		return this.password;
+	}
+}
