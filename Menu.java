41224faa56c133f5bdc2c9cb138eb8dbd87346c1
ezion2 <99609015+ezion2@users.noreply.github.com>
2022-04-04 20:36:12 -0700

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -1,32 +1,28 @@
 //from github user joshwible
+package application;
+
 import java.util.ArrayList;
 
-public class Menu{
+public class Menu
+{
+	
     private ArrayList<Item> menuItems;
-    // private Map<Integer, Item> menuItems;
-    //private ArrayList<Item> results; //search results from searchMenu method
     
-    public Menu(){
-        menuItems = new ArrayList<Item>(); 
-        // menuitems = new HashMap<Integer, Item>();
-
+    public Menu()
+    {
+        this.menuItems = new ArrayList<Item>(); 
     }
     
-   /* public Menu(ArrayList<Item> existingMenu){
-        for(Item item : existingMenu){
-            menuItems.add(item);
-        }
-        results = new ArrayList<Item>();
-    }*/
-    
-    public void addItem(Item item){
-        menuItems.add(item); 
-        //menuItems.put(item.id, item);
+    public void addItem(Item item)
+    {
+    	menuItems.add(item); 
     }
     
     public void removeItem(String name){
-        for(Item item : menuItems){
-            if(name.equals(item.getName())){
+        for(Item item : menuItems)
+        {
+            if(name.equals(item.getName()))
+            {
                 System.out.println(item.getName() + " has been removed from the menu.");
                 menuItems.remove(item);
                 break;
@@ -44,10 +40,13 @@ public class Menu{
         return null;
     }
     
-    public ArrayList<Item> searchMenu(String str){
-        results = new ArrayList<Item>();
-        for(Item item : menuItems){
-            if(item.getInfo().contains(str)){
+    public ArrayList<Item> searchMenu(String str)
+    {
+    	ArrayList<Item> results = new ArrayList<Item>();
+        for(Item item : menuItems)
+        {
+            if(item.getInfo().contains(str))
+            {
                 results.add(item);
             }
         }
